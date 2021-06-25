3a1a2dfd4598ea2191303a2d06066a0c793309f6
Sang Hang <84693219+khbSangHang@users.noreply.github.com>
2021-06-25 16:59:20 +0700

    change store cart session

---
+++ storage/framework/views/3eaeb817106b023f20bbb5df9a9b7aec037d3497.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>E-KSOFT</title>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/font-linearicons.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap-theme.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/jquery.fancybox.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/jquery-ui.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/owl.carousel.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/owl.transitions.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/owl.theme.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/theme.css" media="all"/>
+        <link rel="stylesheet" type="text/css" href="/css/responsive.css" media="all"/>
+        
+    
+        <script type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>
+          <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+          <script type="text/javascript" src="/js/jquery.fancybox.js"></script>
+          <script type="text/javascript" src="/js/jquery-ui.js"></script>
+          <script type="text/javascript" src="/js/owl.carousel.js"></script>
+          <script type="text/javascript" src="/js/theme.js"></script>
+    </head>
+    <body>
+       <div id="app">
+           <app-front></app-front>
+       </div>
+       <script src="/js/app.js" defer></script>
+    </body>
+</html>
+<?php /**PATH D:\xampp\htdocs\KHB-work\e-ksoft\resources\views/index.blade.php ENDPATH**/ ?>
